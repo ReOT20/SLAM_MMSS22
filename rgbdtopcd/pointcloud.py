d29638b2a5704d31557c9493397010b9cb5547aa
Bogdan Rzheusski <bogdanrzheusski@gmail.com>
2022-07-12 00:01:18 +0300

    rgbd to pcd review 1 fixes

--- rgbdtopcd/pointcloud.py
+++ rgbdtopcd/pointcloud.py
@@ -2,23 +2,37 @@ from cv2 import IMREAD_ANYDEPTH
 import open3d as o3d
 import numpy as np
 import cv2
-from math import sqrt
 
-K = np.array([[481.20, 0., 319.50, 0.],
+ImageScaleFactor = 5000
+
+CalibrationMatrix = np.array([[481.20, 0., 319.50, 0.],
                 [0., -480.00, 239.50, 0.],
                 [0., 0., 1., 0.],
                 [0., 0., 0., 1.]])
 
-Kinv = np.linalg.inv(K)
+InvCalibrationMatrix = np.linalg.inv(CalibrationMatrix)
 
-def uvd_xyz(u, v, d):
-    # f = sqrt(K[0][0]**2 + K[1][1]**2)
-    # z = f*sqrt(d**2/
-            #    (u**2 + v**2 + f**2))
-    z = d/5000
-    vector = Kinv@np.array([u, v, 1., 1/z]).transpose()*z
-    return vector[:3]
-    
+def uvd_xyz_array(data):
+    height = data.shape[0]
+    width = data.shape[1]
+    data = data.reshape(height, width, 1)
+    IndiciesArray = np.indices((height, width))
+    IndiciesArray = np.append((IndiciesArray[0].reshape(height, width, 1)),
+                             (IndiciesArray[1].reshape(height, width, 1)),
+                             axis=2)
+    data = np.append(IndiciesArray, data, axis=2)
+    data = data*[1, 1, 1/ImageScaleFactor]
+    data = np.append(data, np.array([[[1]]*width]*height), axis = 2).reshape(height*width, 4)
+    data = data@[[1, 0, 0, 0],
+                 [0, 1, 0, 0],
+                 [0, 0, 0, 1],
+                 [0, 0, 1, 0]]
+    ones = np.array([[1,1,1]]*height*width)
+    raw = np.array([data[:,3]]).T
+    data = data/np.append(ones, raw, axis=1)**2
+    data = (InvCalibrationMatrix@(data.T)).T*raw
+    return data[:, :3]
+
 
 color_raw = np.asarray(cv2.imread('annot.png'))
 depth_raw = np.asarray(cv2.imread('depth.png', IMREAD_ANYDEPTH))
@@ -26,17 +40,12 @@ depth_raw = np.asarray(cv2.imread('depth.png', IMREAD_ANYDEPTH))
 height = color_raw.shape[0]
 width = color_raw.shape[1]
 
-points = np.array([[0.]*3]*height*width)
-colors = np.array([[0.]*3]*height*width)
-
-for i in range(height):
-    for j in range(width):
-        points[width*i + j] = uvd_xyz(i, j, depth_raw[i][j])
-        colors[width*i + j] = color_raw[i][j][::-1]
+points = uvd_xyz_array(depth_raw)
+colors = color_raw.reshape(height*width, 3)[:, ::-1]/256
 
 pcd = o3d.geometry.PointCloud()
 pcd.points = o3d.utility.Vector3dVector(points)
-pcd.colors = o3d.utility.Vector3dVector(colors/256)
+pcd.colors = o3d.utility.Vector3dVector(colors)
 pcd.transform([[1, 0, 0, 0],
                [0, -1, 0, 0],
                [0, 0, -1, 0],
