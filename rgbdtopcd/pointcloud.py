98a2c5e4d7a2da353d6e61a9e43c8feecfff50dd
Bogdan Rzheusski <bogdanrzheusski@gmail.com>
2022-07-22 19:06:50 +0300

    uvd to xyz function rewritten

--- rgbdtopcd/pointcloud.py
+++ rgbdtopcd/pointcloud.py
@@ -6,6 +6,11 @@ import cv2
 
 ImageScaleFactor = 5000
 
+fx = 481.20
+fy = -480.00
+cx = 319.50
+cy = 239.50
+
 
 def uvd_xyz_array(data):
     """Function that converts depth image data to points in 3d space
@@ -18,42 +23,16 @@ def uvd_xyz_array(data):
     """
 
     height, width = data.shape
-    data = data.reshape(height, width, 1)
+    data = data.reshape(height * width)
 
-    # 2d array containing arrays with their coords in 2d array
-    CoordsArray = np.indices((height, width))
-    CoordsArray = np.append(
-        (CoordsArray[0].reshape(height, width, 1)),
-        (CoordsArray[1].reshape(height, width, 1)),
-        axis=2,
-    )
-    # merging coords of points with their depth
-    data = np.append(CoordsArray, data, axis=2)
-    # normalizing depth by image scale factor
-    data = data * [1, 1, 1 / ImageScaleFactor]
-    # extending points data from 3 element arrays to 4 element arrays
-    data = np.append(data, np.array([[[1]] * width] * height), axis=2)
-    # reshaping 3d array into 2d
-    data = data.reshape(height * width, 4)
-    # Swapping 3 and 4 points parameters
-    data = data @ [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 0, 1], [0, 0, 1, 0]]
-    # raising 4-th parameters of points to the power of -1
-    ones = np.array([[1, 1, 1]] * height * width)
-    raw = np.array([data[:, 3]]).T
-    data = data / np.append(ones, raw, axis=1) ** 2
-    # Camera calibrating matrix
-    InvCalibrationMatrix = np.linalg.inv(
-        [
-            [481.20, 0.0, 319.50, 0.0],
-            [0.0, -480.00, 239.50, 0.0],
-            [0.0, 0.0, 1.0, 0.0],
-            [0.0, 0.0, 0.0, 1.0],
-        ]
-    )
-    # multiplying points data by calibration matrix and their 4 parameters
-    data = (InvCalibrationMatrix @ (data.T)).T * raw
-    # First 3 parameters of points are their coords in 3d space
-    return data[:, :3]
+    u = np.repeat(np.arange(height), width)
+    v = np.tile(np.arange(width), height)
+    z = data / ImageScaleFactor
+
+    x = (cx - u) * z / fx
+    y = (cy - v) * z / fy
+
+    return np.column_stack((x, y, z))
 
 
 def main():
